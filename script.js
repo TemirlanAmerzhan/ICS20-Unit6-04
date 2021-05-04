b284922c2cc8cc561bf28d633ccc2f8696074177
Temirlan Amerzhan <>
2021-05-04 13:21:58 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var lol=prompt("Please enter which shape you would like to calculate the area for")
+if(lol==="Trapezoid"){
+    var tr1=Number(prompt("Enter the top base."))
+    var tr2=Number(prompt("Enter the bottom base."))
+    var tr3=Number(prompt("Now enter the height."))
+    confirm((tr1+tr2)*(tr3)/2)
+}
